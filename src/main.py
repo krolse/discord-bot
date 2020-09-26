42ee2f1c52fc919d22938de52e00a03bf2bf6493
Kristoffer Olsen <kris1olsen@gmail.com>
2020-09-26 17:16:42 +0200

    Setup

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import discord
+from definitions import ROOT_PATH
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == '!ping':
+            await message.channel.send('pong')
+
+
+if __name__ == '__main__':
+    def read_token():
+        with open(f'{ROOT_PATH}/token.txt', 'r') as file:
+            return file.read()
+
+    token = read_token()
+    client = MyClient()
+    client.run(token)
