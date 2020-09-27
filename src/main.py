d3b85c7a71d6f731be4e0c49734f89993220552f
Kristoffer Olsen <kris1olsen@gmail.com>
2020-09-27 12:49:29 +0200

    Moved client to own file

--- src/main.py
+++ src/main.py
@@ -1,24 +1,7 @@
-import discord
 from definitions import ROOT_PATH
-
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
-
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-
-        if message.content == '!ping':
-            await message.channel.send('pong')
+from src.client import Client
 
 
 if __name__ == '__main__':
-    def read_token():
-        with open(f'{ROOT_PATH}/token', 'r') as file:
-            return file.read()
-
-    token = read_token()
-    client = MyClient()
-    client.run(token)
+    with open(f'{ROOT_PATH}/token', 'r') as token:
+        Client().run(token.read())
