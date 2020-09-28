a765a02059167641366b915c6ea884ff37ef26df
Kristoffer Olsen <kris1olsen@gmail.com>
2020-09-28 19:26:48 +0200

    Added some simple commands

--- bot/client.py
+++ bot/client.py
@@ -24,8 +24,22 @@ class Client(discord.Client):
         if message.content == f'{PREFIX}ping':
             await message.channel.send('pong')
 
+        if message.content == f'{PREFIX}østers':
+            await message.channel.send('> *Av østers får man sår <:nik:268402644966572032>*')
+            await message.delete()
+
+        if message.content == f'{PREFIX}hsl':
+            with open(f'{ROOT_PATH}/secret', 'r') as secret:
+                await message.channel.send(f'{secret.read()}')
+            await message.delete()
+
         if message.content[1:] in self.sounds:
-            voice_channel = message.author.voice.channel
+            try:
+                voice_channel = message.author.voice.channel
+            except AttributeError:
+                await message.channel.send('You\'re not in a voice channel, ya frig.')
+                await message.delete()
+                return
             if voice_channel:
                 voice = await voice_channel.connect()
                 try:
